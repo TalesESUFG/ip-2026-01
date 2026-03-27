6efe5fd75b241a8caef98b7456a17dd2015e9bef
TalesESUFG <talesmarques@discente.ufg.br>
2026-03-27 16:21:15 -0300

    Renomeação

--- Lista02/Ex07.go
+++ Lista02/Ex07.go
@@ -12,23 +12,29 @@ func main() {
 	f.Scan(&b)
 	f.Println("Qual o número c?")
 	f.Scan(&c)
-	if a < b && b > c {
+	if a > b && b > c {
 		f.Printf("O número intermediário é: %d\n", b)
 		f.Printf("O menor número é: %d\n", c)
-	} else if c < b && b > a {
+		f.Printf("O maior número é: %d\n", a)
+	} else if c > b && b > a {
 		f.Printf("O número intermediário é: %d\n", b)
 		f.Printf("O menor número é: %d\n", a)
-	} else if c < a && a > b {
+		f.Printf("O maior número é: %d\n", c)
+	} else if c > a && a > b {
 		f.Printf("O número intermediário é: %d\n", a)
 		f.Printf("O menor número é: %d\n", b)
-	} else if b < a && a > c {
+		f.Printf("O maior número é: %d\n", c)
+	} else if b > a && a > c {
 		f.Printf("O número intermediário é: %d\n", a)
 		f.Printf("O menor número é: %d\n", c)
-	} else if b < c && c > a {
+		f.Printf("O maior número é: %d\n", b)
+	} else if b > c && c > a {
 		f.Printf("O número intermediário é: %d\n", c)
 		f.Printf("O menor número é: %d\n", a)
-	} else if a < c && c > b {
-		f.Printf("O número intermediário é: %d\n", a)
+		f.Printf("O maior número é: %d\n", b)
+	} else if a > c && c > b {
+		f.Printf("O número intermediário é: %d\n", c)
 		f.Printf("O menor número é: %d\n", b)
+		f.Printf("O maior número é: %d\n", a)
 	}
 }
