25b3244818fbbe924ddf6b47b2ef548f49ca4ba2
TalesESUFG <talesmarques@discente.ufg.br>
2026-03-28 17:29:31 -0300

    Release

--- Lista02/Ex08.go
+++ Lista02/Ex08.go
@@ -9,6 +9,8 @@ func main() {
 	f.Println("Diga um número inteiro e irei verficar se está entre 20 e 90.")
 	f.Scan(&num)
 	if num > 20 && num < 90 {
-		f.Print("O número %d está entre 20 e 90.", num)
+		f.Printf("O número %d está entre 20 e 90.\n", num)
+	} else {
+		f.Printf("O número %d não está entre 20 e 90.\n", num)
 	}
 }
