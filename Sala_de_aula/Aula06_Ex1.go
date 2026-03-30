be5b28c6d0c33802914a6160666c1b6e2946a8fd
Tales <talesmarques@discente.ufg.br>
2026-03-30 20:54:46 -0300

    Criar Aula06_Ex1.go

---
+++ Sala_de_aula/Aula06_Ex1.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    f "fmt"
+)
+
+func main() {
+    const NumNotas int = 3
+    var (
+        nota  [numNotas]float64
+        soma  float64 = 0
+        media float64
+        m     float64
+    )
+    for i := 0; i < numNotas; i++ {
+        f.Printf(Informe a nota%d : ", i)
+        f.Scan(&nota[i])
+        soma += nota[i]
+    }
+    m = 0
+    for i, v := range nota {
+        f.Printf("Nota %d = %f\n", i, v)
+        m = m + v
+    }
+    media = m / float64(numNotas)
+    f.Printf("Média das notas: %.2f\n", media)
+    if media < 6 {
+        f.Println("Você reprovou!")
+    } else {
+        f.Println("Você Passou!")
+    }
+}
