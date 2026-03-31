f38016c1180484c433ffcd294bed0f00383bad07
Tales <talesmarques@discente.ufg.br>
2026-03-30 21:34:58 -0300

    Atualizar o Aula06_Ex3.go

--- Sala_de_aula/Aula06_Ex3.go
+++ Sala_de_aula/Aula06_Ex3.go
@@ -9,8 +9,8 @@ func main() {
         num [10]int
     )
     for i := 0; i < len (num); i++ {
-        f. Printf ("Informe o %d valor : ", i+1)
-        f. Scan (&num[i])
+        f.Printf("Informe o %d valor : ", i+1)
+        f.Scan(&num[i])
     }
     for i := 10; i >= 0; i-- {
         f.Println(i, " ")
