b70873395aa6a7cc4b0bca780ec2c2504aedeea1
TalesESUFG <talesmarques@discente.ufg.br>
2026-03-31 13:33:29 -0300

    Release

--- Lista02/Ex20.go
+++ Lista02/Ex20.go
@@ -10,7 +10,7 @@ func main() {
 		con   string
 		op    float64
 	)
-	f.Println("Olá! Qual é o valor do produto e o método de pagamento?(A vista e credito, dinheiro, cheque; ou parcela 2x e parcela 3x)")
+	f.Println("Olá! Qual é o valor do produto e o método de pagamento?(A vista e credito, dinheiro, cheque; ou parcela2x e parcela3x)")
 	f.Scan(&valor, &con)
 	if con == "A vista" || con == "a vista" && con == "Cheque" || con == "cheque" || con == "Dinheiro" || con == "dinheiro" {
 		op = valor * 0.9
@@ -18,11 +18,15 @@ func main() {
 	} else if con == "A vista" || con == "a vista" && con == "Cartão" || con == "cartão" {
 		op = valor * 0.95
 		f.Printf("O valor da compra será de : %.2f\n", op)
-	} else if con == "Parcela 2x" || con == "parcela 2x" {
+	} else if con == "Parcela2x" || con == "parcela2x" {
 		op = valor / 2
 		f.Printf("O valor da compra será de 2x : %.2f\n", op)
-	} else if con == "Parcela 3x" || con == "parcela 3x" {
+	} else if con == "Parcela3x" || con == "parcela3x" {
 		op = (valor * 1.1) / 3
 		f.Printf("O valor da compra será de 3x : %.2f\n", op)
+	} else if valor == 0 {
+		f.Println("Erro! Valor inválido")
+	} else {
+		f.Println("Erro! Método de pagamento inválido")
 	}
 }
