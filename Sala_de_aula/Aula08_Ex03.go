eaa27eae0240d2b5c731bb879bb1486db35f6ba1
Tales <talesmarques@discente.ufg.br>
2026-04-01 21:08:55 -0300

    Create Aula08_Ex03.go

---
+++ Sala_de_aula/Aula08_Ex03.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	f "fmt"
+)
+
+func main() {
+	var (
+		x int
+	)
+	f.Println("Escreva 1 números inteiro para calcular seu fatorial.")
+	f.Scan(&x)
+	l := fatorial(x)
+	f.Printf("O fatorial é: %d\n", l)
+}
+func fatorial(num1 int) int {
+	var s int = 1
+	for num1 > 1 {
+		s = num1 * s
+		num1--
+	}
+	return s
+}
