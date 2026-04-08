32f69063b3bbff47f698ea97364cc349ff89be7a
Tales M. <talesmarques@discente.ufg.br>
2026-04-07 21:17:34 -0300

    Update Ex02.go

--- Exercicios_LivroFPC/Ex02.go
+++ Exercicios_LivroFPC/Ex02.go
@@ -6,9 +6,10 @@ func main() {
 	var (
 		n1 float64
 		n2 float64
+		n3 float64
 	)
-	f.Print("Digite dois números positivos e irei informar a subtração do primeiro pelo segundo : ")
-	f.Scan(&n1,&n2)
-	n1 -= n2
-	f.Printf("O resultado da subtração é : %.2f\n", n1)
+	f.Print("Informe três números e retornarei sua multiplicação : ")
+	f.Scan(&n1,&n2,&n3)
+	m := n1 * n2 * n3
+	f.Printf("O resultado da multiplicação é : ", m)
 }
