38f1a959fbd91229f574b374f55a504b043cd0f6
Tales M. <talesmarques@discente.ufg.br>
2026-04-07 21:06:12 -0300

    Create Ex02.go

---
+++ Exercicios_LivroFPC/Ex02.go
@@ -0,0 +1,12 @@
+package main
+import f "fmt"
+func main() {
+	var (
+		n1 float64
+		n2 float64
+	)
+	f.Print("Digite dois números positivos e irei informar a subtração do primeiro pelo segundo : ")
+	f.Scan(&n1,&n2)
+	n1-=n2
+	f.Printf("O resultado da subtração é : %.2f\n",n1)
+}
