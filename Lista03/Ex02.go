fa8c89673cfb0d8e9f2b53d05038447c4ea5de2c
TalesESUFG <talesmarques@discente.ufg.br>
2026-04-09 12:14:30 -0300

    Release

--- Lista03/Ex02.go
+++ Lista03/Ex02.go
@@ -4,15 +4,14 @@ import f "fmt"
 
 func main() {
 	var mer float64
-	i := 1
-	m := 50
-	var v int
-	for m < 72 {
-		v2 := m + 2
+	var i int = 0
+	var v int = 0
+	for m := 50; m <= 70; m += 2 {
+		v += m
 		i++
-		v = m + v2
+		f.Printf(" %d\n", v)
 	}
 	mer = float64(v) / float64(i)
-	f.Printf("o resultado da soma dos valores pares é : %d\n", v)
+	f.Printf("o resultado da soma dos valores pares é : %d\n", i)
 	f.Printf("o resultado da média aritimética entre todos os valores pares do 50 ao 70 é : %.2f\n", mer)
 }
