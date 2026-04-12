6c7cc81b6f126d0cd80fd4d83cd68f92c92f42cc
TalesESUFG <talesmarques@discente.ufg.br>
2026-04-12 13:47:06 -0300

    Release

--- Lista03/Ex12.go
+++ Lista03/Ex12.go
@@ -10,14 +10,14 @@ func fac2(x int) int {
 }
 func main() {
 	var n float64
-	f.Print("Digite um número N real  e irei fazer o somatório: ")
+	f.Print("Digite um número N real e irei fazer o somatório: ")
 	f.Scan(&n)
 	s := 0.0
 	for i := 1; i <= 20; i++ {
 		r := n / float64(fac2(i))
 		s += r
 		if i == 20 {
-			f.Printf("%.20f ", s)
+			f.Printf("O somátorio é : %.20f\n", s)
 		}
 	}
 }
