b7bcbe6c2797ea7f19cffd63818b450209046b7a
TalesESUFG <talesmarques@discente.ufg.br>
2026-04-14 14:54:58 -0300

    Release

--- Lista03/Ex25.go
+++ Lista03/Ex25.go
@@ -3,11 +3,24 @@ package main
 import f "fmt"
 
 func main() {
-        var r float64
-        for i := 1.0; i != 16384; i *= 2 {
-                n := 225.0
-                m := i/n
-                r += m
-        }
-        f.Print("O resultado é : ", r)
+	var q []float64
+	x := 225.0
+	x2 := 29.0
+	var r float64
+	for i := 1.0; i <= 16384; i *= 2 {
+		r = i / x
+		x -= x2
+		x2 -= 2
+		q = append(q, r)
+	}
+	s := 0.0
+	for z := 0; z < 14; z++ {
+		s = q[0]
+		if z%2 == 0 {
+			s -= q[z+1]
+		} else {
+			s += q[z+1]
+		}
+	}
+	f.Println("O valor final é : ", s)
 }
---
+++ Lista03/Ex26.go
@@ -0,0 +1,20 @@
+package main
+
+import f "fmt"
+
+func fac(x int) int {
+	if x == 0 {
+		return 1
+	}
+	return x * fac(x-1)
+}
+func main() {
+	v := 100.0
+	s := 0.0
+	for i := 0; i < 20; i++ {
+		k := float64(fac(i))
+		s += (v / k)
+		v -= 1
+	}
+	f.Print("O resultado é : ", s)
+}
