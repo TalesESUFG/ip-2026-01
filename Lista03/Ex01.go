79b7207994dc8711ae4b17134be8690be77c3147
Tales M. <talesmarques@discente.ufg.br>
2026-04-13 21:30:37 -0300

    Update Ex01.go

--- Lista03/Ex01.go
+++ Lista03/Ex01.go
@@ -13,9 +13,9 @@ func main() {
 	if e < 0 {
 		b = 1 / b
 		e = -e
-	} // como estou arredondando por duas casas decimais, muito provavelmente não irá demonstrar a maioria das exponenciações negativas.
+	}
 	for i := 0; i < e; i++ {
 		r *= b
 	}
-	f.Printf("O resultado desta potência é : %.2f\n", r)
+	f.Print("O resultado desta potência é : ", r, "\n")
 }
