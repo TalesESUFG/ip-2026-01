52f0ebb07fadebf928210411cf1b9402bab45725
TalesESUFG <talesmarques@discente.ufg.br>
2026-04-16 13:25:50 -0300

    Release

--- Lista03/Ex26.go
+++ Lista03/Ex26.go
@@ -6,13 +6,13 @@ func fac3(x int) int {
 	if x == 0 {
 		return 1
 	}
-	return x * fac(x-1)
+	return x * fac3(x-1)
 }
 func main() {
 	v := 100.0
 	s := 0.0
 	for i := 0; i < 20; i++ {
-		k := float64(fac(i))
+		k := float64(fac3(i))
 		s += (v / k)
 		v -= 1
 	}
