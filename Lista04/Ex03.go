812c8438339d0589faaf959d07e74dbd876083fa
Tales M. <talesmarques@discente.ufg.br>
2026-04-16 20:08:01 -0300

    Add reverse function and main logic for array input
    Implement a function to reverse an array of integers.

---
+++ Lista04/Ex03.go
@@ -0,0 +1,27 @@
+package main
+
+import f "fmt"
+
+func reverso(s,e int, q []int) {
+  if s>e{
+    return
+  }
+  q[s], q[e] = q[e],q[s]
+  reverso(s+1, e-1,q)
+}
+func main() {
+	var e int
+	var q[]int
+	f.Print("informe o número de elementos : ")
+	f.Scan(&e)
+	for i:= 0; i < e;i++ {
+		var n int
+		f.Printf("Digite o %dº número : ", i+1)
+		f.Scan(&n)
+		q = append(q, n)
+	}
+	s := 0
+	en := len(q)-1
+	reverso(s,en,q)
+	f.Println(q)
+}
