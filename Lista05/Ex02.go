3115bec6f022e5de33c7565a1d8281565fb47625
TalesESUFG <talesmarques@discente.ufg.br>
2026-05-01 13:42:07 -0300

    Release

--- Lista05/Ex02.go
+++ Lista05/Ex02.go
@@ -3,19 +3,40 @@ package main
 import f "fmt"
 
 func main() {
-	var l1, l2 []int
+	var l1, l2, par, impar, r1, r2 []int
+	f.Print("Digite os 10 números da lista 1 : ")
 	for i := 0; i < 10; i++ {
 		var n int
-		f.Printf("Digite o %dº número da lista 1 : ", i+1)
 		f.Scan(&n)
 		l1 = append(l1, n)
 	}
+	f.Print("Digite o 5 números da lista 2 : ")
 	for i := 0; i < 5; i++ {
 		var n int
-		f.Printf("Digite o %dº número da lista 2 : ", i+1)
 		f.Scan(&n)
 		l2 = append(l2, n)
 	}
-	f.Println(l1)
-	f.Println(l2)
+	for i := 0; i < len(l1); i++ {
+		if l1[i]%2 == 0 {
+			par = append(par, l1[i])
+		} else {
+			impar = append(impar, l1[i])
+		}
+	}
+	for i := 0; i < len(par); i++ {
+		var m int
+		for y := 0; y < len(l2); y++ {
+			m += l2[y]
+		}
+		r1 = append(r1, m+par[i])
+	}
+	f.Println(r1)
+	for i := 0; i < len(impar); i++ {
+		var m int
+		for y := 0; y < len(l2); y++ {
+			m += l2[y]
+		}
+		r2 = append(r2, m+impar[i])
+	}
+	f.Println(r2)
 }
